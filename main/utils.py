dc91f77c6ff9fa1a8f3dab770a7ffdfd1eb34792
Matthew Khoo <matthew.khoo.mk@gmail.com>
2021-10-28 02:14:22 +0800

    packaged encoder and pipeline to utils.py

--- main/utils.py
+++ main/utils.py
@@ -1,1 +1,16 @@
-import sklearn as sk
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+def create_cat_encoder(cols_cat: list)->ColumnTransformer:
+    cat_encoder = ColumnTransformer([("cat", OrdinalEncoder(), cols_cat)], remainder='passthrough')
+    return cat_encoder
+
+def create_pipeline(pipe: list)->Pipeline:
+    pipeline = Pipeline(pipe)
+    return pipeline
+
+def pipeline_fit_transform(pipeline: Pipeline, data):
+    return pipeline.fit_transform(data)
+
+
