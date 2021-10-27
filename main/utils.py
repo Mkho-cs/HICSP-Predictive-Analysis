a094caa73d064e99feddef3116239834ee90776f
Matthew Khoo <matthew.khoo.mk@gmail.com>
2021-10-28 03:21:53 +0800

    Wrote predictive modelling utils skeleton

--- main/utils.py
+++ main/utils.py
@@ -1,7 +1,11 @@
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OrdinalEncoder, MinMaxScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import roc_auc_score
 
+"""
+Data preprocessing
+"""
 def create_cat_encoder(cols_cat: list)->ColumnTransformer:
     cat_encoder = ColumnTransformer([("cat", OrdinalEncoder(), cols_cat)], remainder='passthrough')
     return cat_encoder
@@ -13,4 +17,13 @@ def create_pipeline(pipe: list)->Pipeline:
 def pipeline_fit_transform(pipeline: Pipeline, data):
     return pipeline.fit_transform(data)
 
+def create_minmax_scaler():
+    return MinMaxScaler()
+
+"""
+Predictive modelling
+"""
+
+def train_val_score(model, train: dict, val: dict, output=roc_auc_score, fit=True)->tuple:
+    if fit: model.fit(train['x'], train['y'])
 
